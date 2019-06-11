d7a70a39ab57ed3e77b04264622fc1fe46bc2e68
cp <sampoop23@163.com>
2019-06-11 11:30:56 +0800

    代理设置

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://api.auauz.net"'
-  BASE_API: '"http://120.78.207.146:8000"'
+  // BASE_API: '"http://120.78.207.146:8000"'
+  BASE_API: '"/api"'
 }
